0843b46cb02c7cba4443dd451306a46e96492360
Megha-2600705038 <megha.619061@gmail.com>
2026-08-14 21:36:01 +0530

    Added basic syntax and variable practice

---
+++ python/pythin_basic.py
@@ -0,0 +1,129 @@
+print("Hello world")
+
+if 10>2:
+    print("10 is larger")
+
+
+print("I'm Megha")
+print("A MSc Computer Science student studying in the Central University Of Kerala!")
+
+print('Hai,Do not give up')
+
+print("Hello,",end=" ")
+print("I m going to print on the same line")
+
+
+print(3+1)
+print(50-39)
+print(2*3)
+print(30/10)
+
+
+#-----------#VARIABLES----------
+#x = 10
+#x = 'Megha'
+#print(x)
+
+#x = int(3)
+#y = str('Megha')
+#z = float(7)
+#print(y)
+#print(x)
+#print(z)
+
+x = 4
+print(type(x))
+
+a = 10
+A = 5
+print(a)
+print(A)
+
+
+r,s,p = 10,20,30
+print(r)
+print(s)
+print(p)
+
+f = g = h = 'Megha'
+print(f)
+print(g)
+print(h)
+
+v = ['car','bike','scooty']
+p,i,o = 'car','bike','scooty'
+print(p)
+print(i)
+print(o)
+
+
+b,c,d = 'Hai', 'Your', 'Name'
+print(b + c + d)
+print(b, c, d)
+
+
+s = 3
+l = 3.1
+i = 1j
+
+m = complex(s)
+print(m)
+
+hi = 'developer'
+def myfun():
+    print('You are a ' + hi)
+
+myfun()
+
+hy = 'great'
+
+def myfun1():
+    hy = 'wonderful'
+    print("you are " + hy)
+
+myfun1()
+print('you are ' + hy)
+
+
+he = 'beautiful'
+def myfun2():
+   
+    global he
+    he = 'superbe'
+
+myfun2()
+print(he)
+
+my = 'Hello, World'
+print(my[1:5])
+
+my1 = 'Hello, World'
+print(my1[:3])
+
+my2 = 'Hello, Wolrd'
+print(my2[4:])
+
+my3 = 'Hello, World'
+print(my3.split(', '))
+
+my4 = ' Hello World'
+print(my4.strip())
+
+my5 = 'Hello World'
+print(my5.replace('H','w'))
+
+my6 = 'Hello WOrld'
+print(my6.upper())
+
+my7 = 'Hello World'
+print(my7.lower())
+
+str1 = 'Hello'
+str2 = 'World'
+print(str1 + str2)
+
+
+theList = ["apple","orange","banana"]
+print(theList)
+print(len(theList))
+print(theList[0])
