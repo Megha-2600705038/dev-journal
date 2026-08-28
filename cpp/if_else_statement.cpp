5d5d7f6bf588733340de2cf19c2d6a0afa31bf7e
Megha-2600705038 <megha.619061@gmail.com>
2026-08-28 09:50:42 +0530

    Added if_else_statement

---
+++ cpp/if_else_statement.cpp
@@ -0,0 +1,16 @@
+//Example for if - else statement:
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int number;
+
+    cout<<"Enter the number : ";
+    cin>>number;
+
+    if(number%2 == 0)
+        cout<<"Number is even";
+    else
+        cout<<"Number is odd";
+}
